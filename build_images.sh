31bb47b3b81deaa3d941f47a989197063f45957b
Dan Mons <dan@mons.com.au>
2021-10-28 15:46:00 +1000

    added RGB triple description to composited images

--- build_images.sh
+++ build_images.sh
@@ -30,6 +30,7 @@ esac
 
 I="convert -colorspace sRGB -type truecolor -depth 8"
 M="montage +set label -colorspace sRGB -type truecolor -depth 8"
+D="${I} -gravity NorthEast -background black -fill gray -pointsize 30 -annotate +10+10"
 
 # Set our chunk sizes and final resolutions
 CXRES=$(( ${XRES}/4 ))
@@ -46,60 +47,134 @@ echo "Building image chunks..."
 ## Colour ratios taken from ColorHCFR in 0-255 (full range RGB) mode
 # Solid colours
 $I -size "${CRES}" xc:"rgb(0,0,0)"        ${1}/chunk_black.tif
+$D "0,0,0" ${1}/chunk_black.tif ${1}/desc_black.tif
+
 $I -size "${CRES}" xc:"rgb(255,255,255)"  ${1}/chunk_white.tif
+$D "255,255,255" ${1}/chunk_black.tif ${1}/desc_white.tif
+
 $I -size "${CRES}" xc:"rgb(255,0,0)"      ${1}/chunk_red.tif
+$D "255,0,0" ${1}/chunk_black.tif ${1}/desc_red.tif
+
 $I -size "${CRES}" xc:"rgb(0,255,0)"      ${1}/chunk_green.tif
+$D "0,255,0" ${1}/chunk_black.tif ${1}/desc_green.tif
+
 $I -size "${CRES}" xc:"rgb(0,0,255)"      ${1}/chunk_blue.tif
+$D "0,0,255" ${1}/chunk_black.tif ${1}/desc_blue.tif
+
 $I -size "${CRES}" xc:"rgb(0,255,255)"    ${1}/chunk_cyan.tif
+$D "0,255,255" ${1}/chunk_black.tif ${1}/desc_cyan.tif
+
 $I -size "${CRES}" xc:"rgb(255,0,255)"    ${1}/chunk_magenta.tif
+$D "255,0,255" ${1}/chunk_black.tif ${1}/desc_magenta.tif
+
 $I -size "${CRES}" xc:"rgb(255,255,0)"    ${1}/chunk_yellow.tif
+$D "255,255,0" ${1}/chunk_black.tif ${1}/desc_yellow.tif
 
 # Grey scale
 $I -size "${CRES}" xc:"rgb(26,26,26)"     ${1}/chunk_grey10.tif
+$D "26,26,26" ${1}/chunk_black.tif ${1}/desc_grey10.tif
+
 $I -size "${CRES}" xc:"rgb(51,51,51)"     ${1}/chunk_grey20.tif
+$D "51,51,51" ${1}/chunk_black.tif ${1}/desc_grey20.tif
+
 $I -size "${CRES}" xc:"rgb(77,77,77)"     ${1}/chunk_grey30.tif
+$D "77,77,77" ${1}/chunk_black.tif ${1}/desc_grey30.tif
+
 $I -size "${CRES}" xc:"rgb(102,102,102)"  ${1}/chunk_grey40.tif
+$D "102,102,102" ${1}/chunk_black.tif ${1}/desc_grey40.tif
+
 $I -size "${CRES}" xc:"rgb(128,128,128)"  ${1}/chunk_grey50.tif
+$D "128,128,128" ${1}/chunk_black.tif ${1}/desc_grey50.tif
+
 $I -size "${CRES}" xc:"rgb(153,153,153)"  ${1}/chunk_grey60.tif
+$D "153,153,153" ${1}/chunk_black.tif ${1}/desc_grey60.tif
+
 $I -size "${CRES}" xc:"rgb(178,178,178)"  ${1}/chunk_grey70.tif
+$D "178,178,178" ${1}/chunk_black.tif ${1}/desc_grey70.tif
+
 $I -size "${CRES}" xc:"rgb(204,204,204)"  ${1}/chunk_grey80.tif
+$D "204,204,204" ${1}/chunk_black.tif ${1}/desc_grey80.tif
+
 $I -size "${CRES}" xc:"rgb(229,229,229)"  ${1}/chunk_grey90.tif
+$D "229,229,229" ${1}/chunk_black.tif ${1}/desc_grey90.tif
 
 # Red saturations
 $I -size "${CRES}" xc:"rgb(127,127,127)"  ${1}/chunk_red00.tif
+$D "127,127,127" ${1}/chunk_black.tif ${1}/desc_red00.tif
+
 $I -size "${CRES}" xc:"rgb(171,112,112)"  ${1}/chunk_red25.tif
+$D "171,112,112" ${1}/chunk_black.tif ${1}/desc_red25.tif
+
 $I -size "${CRES}" xc:"rgb(204,93,93)"    ${1}/chunk_red50.tif
+$D "204,93,93" ${1}/chunk_black.tif ${1}/desc_red50.tif
+
 $I -size "${CRES}" xc:"rgb(231,68,68)"    ${1}/chunk_red75.tif
+$D "231,68,68" ${1}/chunk_black.tif ${1}/desc_red75.tif
 
 # Green saturations
 $I -size "${CRES}" xc:"rgb(219,219,219)"  ${1}/chunk_green00.tif
+$D "219,219,219" ${1}/chunk_black.tif ${1}/desc_green00.tif
+
 $I -size "${CRES}" xc:"rgb(177,234,177)"  ${1}/chunk_green25.tif
+$D "177,234,177" ${1}/chunk_black.tif ${1}/desc_green25.tif
+
 $I -size "${CRES}" xc:"rgb(137,243,137)"  ${1}/chunk_green50.tif
+$D "137,243,137" ${1}/chunk_black.tif ${1}/desc_green50.tif
+
 $I -size "${CRES}" xc:"rgb(94,250,94)"    ${1}/chunk_green75.tif
+$D "rgb(94,250,94" ${1}/chunk_black.tif ${1}/desc_green75.tif
 
 # Blue saturations
 $I -size "${CRES}" xc:"rgb(78,78,78)"     ${1}/chunk_blue00.tif
+$D "78,78,78" ${1}/chunk_black.tif ${1}/desc_blue00.tif
+
 $I -size "${CRES}" xc:"rgb(76,78,100)"    ${1}/chunk_blue25.tif
+$D "76,78,100" ${1}/chunk_black.tif ${1}/desc_blue25.tif
+
 $I -size "${CRES}" xc:"rgb(72,72,128)"    ${1}/chunk_blue50.tif
+$D "72,72,128" ${1}/chunk_black.tif ${1}/desc_blue50.tif
+
 $I -size "${CRES}" xc:"rgb(64,64,169)"    ${1}/chunk_blue75.tif
+$D "64,64,169" ${1}/chunk_black.tif ${1}/desc_blue75.tif
 
 # Yellow saturations
 $I -size "${CRES}" xc:"rgb(247,247,247)"  ${1}/chunk_yellow00.tif
+$D "247,247,247" ${1}/chunk_black.tif ${1}/desc_yellow00.tif
+
 $I -size "${CRES}" xc:"rgb(249,249,205)"  ${1}/chunk_yellow25.tif
+$D "249,249,205" ${1}/chunk_black.tif ${1}/desc_yellow25.tif
+
 $I -size "${CRES}" xc:"rgb(252,252,162)"  ${1}/chunk_yellow50.tif
+$D "252,252,162" ${1}/chunk_black.tif ${1}/desc_yellow50.tif
+
 $I -size "${CRES}" xc:"rgb(254,254,113)"  ${1}/chunk_yellow75.tif
+$D "254,254,113" ${1}/chunk_black.tif ${1}/desc_yellow75.tif
 
 # Cyan Saturations
 $I -size "${CRES}" xc:"rgb(229,229,229)"  ${1}/chunk_cyan00.tif
+$D "229,229,229" ${1}/chunk_black.tif ${1}/desc_cyan00.tif
+
 $I -size "${CRES}" xc:"rgb(201,236,236)"  ${1}/chunk_cyan25.tif
+$D "201,236,236" ${1}/chunk_black.tif ${1}/desc_cyan25.tif
+
 $I -size "${CRES}" xc:"rgb(168,242,242)"  ${1}/chunk_cyan50.tif
+$D "168,242,242" ${1}/chunk_black.tif ${1}/desc_cyan50.tif
+
 $I -size "${CRES}" xc:"rgb(122,249,249)"  ${1}/chunk_cyan75.tif
+$D "122,249,249" ${1}/chunk_black.tif ${1}/desc_cyan75.tif
 
 # Magenta Saturations
 $I -size "${CRES}" xc:"rgb(144,144,144)"  ${1}/chunk_magenta00.tif
+$D "144,144,144" ${1}/chunk_black.tif ${1}/desc_magenta00.tif
+
 $I -size "${CRES}" xc:"rgb(165,136,165)"  ${1}/chunk_magenta25.tif
+$D "165,136,165" ${1}/chunk_black.tif ${1}/desc_magenta25.tif
+
 $I -size "${CRES}" xc:"rgb(189,122,189)"  ${1}/chunk_magenta50.tif
+$D "189,122,189" ${1}/chunk_black.tif ${1}/desc_magenta50.tif
+
 $I -size "${CRES}" xc:"rgb(218,98,218)"   ${1}/chunk_magenta75.tif
+$D "218,98,218" ${1}/chunk_black.tif ${1}/desc_magenta75.tif
 
 echo "Compositing images..."
 
@@ -115,7 +190,7 @@ do
     ${1}/chunk_black.tif ${1}/chunk_black.tif ${1}/chunk_black.tif ${1}/chunk_black.tif \
     ${1}/chunk_black.tif ${1}/chunk_${COLOUR}.tif ${1}/chunk_${COLOUR}.tif ${1}/chunk_black.tif \
     ${1}/chunk_black.tif ${1}/chunk_${COLOUR}.tif ${1}/chunk_${COLOUR}.tif ${1}/chunk_black.tif \
-    ${1}/chunk_black.tif ${1}/chunk_black.tif ${1}/chunk_black.tif ${1}/chunk_black.tif \
+    ${1}/chunk_black.tif ${1}/desc_${COLOUR}.tif ${1}/chunk_black.tif ${1}/chunk_black.tif \
     -tile 4x4 -geometry +0+0 ${1}/comp_${COLOUR}.tif
 done
 
